ce0c732c3557c2209f4ae5948fcdb2a7d1a890e5
Lasha Lomidze <lomidzelashaf@gmail.com>
2022-04-14 01:52:30 +0400

    api-endpoint-development: Implement per country statistics summary methods Implement mutators for country model fields Add comments to methods

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -13,6 +14,80 @@ class Country extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+
+    /**
+     * @return mixed
+     * @author Lasha Lomidze <lomidzelashaf@gmail.com>
+     * Selects and returns countries with respective statistics
+     * totals. Runs fast enough ~50ms, but probably can be improved
+     * to avoid generated subquery usage.
+     */
+    public static function getSummaryPerCountry()
+    {
+        return self::withSum('statistics', 'deaths')
+            ->withSum('statistics', 'confirmed')
+            ->withSum('statistics', 'recovered')
+            ->get();
+    }
+
+    /**
+     * @return Attribute
+     * @author Lasha Lomidze <lomidzelashaf@gmail.com>
+     * A mutator for name column, to return pristine values
+     * instead of redundantly encoded ones.
+     */
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => json_decode($value, true));
+    }
+
+    /**
+     * @return Attribute
+     * @author Lasha Lomidze <lomidzelashaf@gmail.com>
+     * Generic mutator for replacing null values with numeric
+     * integer 0s.
+     */
+    protected function replaceNull(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value === null ? 0 : (int)$value
+        );
+    }
+
+    /**
+     * @return Attribute
+     * @author Lasha Lomidze <lomidzelashaf@gmail.com>
+     * Mutator for death sums field. Essentially replaces null
+     * with 0 if no records found and nothing to sum there.
+     */
+    protected function statisticsSumDeaths(): Attribute
+    {
+        return $this->replaceNull();
+    }
+
+    /**
+     * @return Attribute
+     * @author Lasha Lomidze <lomidzelashaf@gmail.com>
+     * Mutator for confirmed sums field. Essentially replaces null
+     * with 0 if no records found and nothing to sum there.
+     */
+    protected function statisticsSumConfirmed(): Attribute
+    {
+        return $this->replaceNull();
+    }
+
+    /**
+     * @return Attribute
+     * @author Lasha Lomidze <lomidzelashaf@gmail.com>
+     * Mutator for recovered sums field. Essentially replaces null
+     * with 0 if no records found and nothing to sum there.
+     */
+    protected function statisticsSumRecovered(): Attribute
+    {
+        return $this->replaceNull();
+    }
 
     /**
      * @return HasMany
@@ -26,6 +101,7 @@ class Country extends Model
     /**
      * @return HasOne
      * @author Lasha Lomidze <lomidzelashaf@gmail.com>
+     * Relational method for fetching today's statistics for a country.
      */
     public function todayStatistic(): HasOne
     {
